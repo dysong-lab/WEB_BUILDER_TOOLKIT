d948c7fe3e7aabfd80a90068d9424bb64df351a0
DYSONG <dysong@wemb.co.kr>
2026-04-20 16:06:11 +0900

    Add ACBmetasol component

---
+++ RNBT_architecture/DesignComponentSystem/Components/3D_Components/ACBmetasol/Standard/scripts/register.js
@@ -0,0 +1,55 @@
+/**
+ * ACBmetasol — Standard / scripts / register
+ *
+ * MeshStateMixin 단독 적용
+ * 상태에 따라 8개 메시 색상 변경
+ */
+
+const MESH_NAMES = [
+    'DDH003',
+    'DDH005',
+    'DDH001',
+    'DDH002',
+    'DDH021',
+    'Object231',
+    'Rectangle070',
+    'Line005'
+];
+
+applyMeshStateMixin(this, {
+    colorMap: {
+        normal:  0x34d399,
+        warning: 0xfbbf24,
+        error:   0xf87171,
+        offline: 0x6b7280
+    }
+});
+
+const baseRenderData = this.meshState.renderData;
+
+this.meshState.renderData = ({ response }) => {
+    if (!Array.isArray(response)) return;
+
+    const expanded = response.flatMap((item) =>
+        MESH_NAMES.map((meshName) => ({
+            meshName,
+            status: item.status
+        }))
+    );
+
+    baseRenderData({ response: expanded });
+};
+
+this.subscriptions = {
+    equipmentStatus: [this.meshState.renderData]
+};
+
+const { subscribe } = GlobalDataPublisher;
+const { each, go } = fx;
+
+go(
+    Object.entries(this.subscriptions),
+    each(([topic, handlers]) =>
+        each(handler => subscribe(topic, this, handler), handlers)
+    )
+);
